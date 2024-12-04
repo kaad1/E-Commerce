be464f6536e4f806af23049a8c458457ee8e2a03
elgiganten <elgiganten@DESKTOP-T8PMFC9>
2024-12-04 14:17:27 +0100

    Creating Categories

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            this._db = db;
+        }
+        public async Task <IActionResult> Index()
+        {
+           List<Category> objCategoryList=await _db.Categories.ToListAsync();
+           
+           return View(objCategoryList);
+        }
+    }
+}
